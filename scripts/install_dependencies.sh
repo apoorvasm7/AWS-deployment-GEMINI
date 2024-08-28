76865fd689428fcbf1400d2b1266a132fc5ac113
Apoorva SM <apoorvasm777@gmail.com>
2024-08-28 18:16:21 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cd /home/ec2-user/Gemini-AI
-pip install -r requirements.txt
+# Install Python and pip if not installed
+sudo yum install -y python3
+python3 -m ensurepip --upgrade
+
+# Navigate to the project directory
+cd /home/ec2-user/Gemini-AI || exit
+
+# Install dependencies
+pip3 install -r requirements.txt
